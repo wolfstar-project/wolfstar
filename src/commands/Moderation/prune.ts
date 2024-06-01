b3384812620cde9487ec0d71273480e10c0cac65
RedStars071 <redtwoghost@gmail.com>
2024-06-01 15:11:29 +0200

    fix: resolved prune command

--- src/commands/Moderation/prune.ts
+++ src/commands/Moderation/prune.ts
@@ -8,9 +8,10 @@ import { getLogger, sendTemporaryMessage } from '#utils/functions';
 import { getImageUrl } from '#utils/util';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Args, Argument, CommandOptionsRunTypeEnum } from '@sapphire/framework';
-import { isNullOrUndefined, isNullish } from '@sapphire/utilities';
+import { filterNullAndUndefined, isNullish } from '@sapphire/utilities';
 import { Collection, PermissionFlagsBits, RESTJSONErrorCodes, Message, type PartialMessage, TextChannel, type Snowflake } from 'discord.js';
 import { chunk } from '@sapphire/iterator-utilities';
+import { setTimeout } from 'node:timers/promises';
 
 const enum Position {
 	Before,
@@ -150,16 +151,18 @@ export class UserCommand extends WolfSubcommand {
 		if (silent && messages.size !== 100) {
 			messages.set(message.id, message);
 		}
+
+		const filteredKeys = this.resolveKeys([...messages.keys()], position, limit);
 		if (silent) return null;
 
-		const content = args.t(LanguageKeys.Commands.Moderation.PruneAlert, { count: messages.size, total: limit });
+		const content = args.t(LanguageKeys.Commands.Moderation.PruneAlert, { count: filteredKeys.length, total: limit });
 		return sendTemporaryMessage(message, content, seconds(10));
 	}
 
 	private async purgeBulk(
 		message: GuildMessage,
 		options: { limit: number; before?: string; after?: string; filter: BooleanFn<[GuildMessage]> }
-	): Promise<Collection<string, GuildMessage | PartialMessage>> {
+	): Promise<Collection<string, Message | PartialMessage>> {
 		const { channel } = message;
 
 		const { before, after, filter: filtered, limit } = options;
@@ -169,26 +172,30 @@ export class UserCommand extends WolfSubcommand {
 		};
 		let remainingLimit = limit;
 		const toDelete: string[] = [];
-		const msgDeleted = new Collection<string, GuildMessage | PartialMessage>();
+		const msgDeleted = new Collection<string, Message | PartialMessage>();
 		const logger = getLogger(message.guildId);
 		let hasError = false;
 
 		logger.prune.set(message.channelId, { userId: message.author.id });
 		const BulkDelete = async (messageIds: string[]) => {
 			for (const chunked of chunk(messageIds, 100)) {
-				let msgs = new Collection<Snowflake, Message | PartialMessage | undefined>();
+				const messagesBulked = new Collection<Snowflake, Message | PartialMessage | undefined>();
 				try {
-					msgs = await (message.channel as TextChannel).bulkDelete(chunked);
+					messagesBulked.concat(await (message.channel as TextChannel).bulkDelete(chunked));
 				} catch (error) {
 					logger.prune.unset(message.channelId);
 					hasError = true;
 					if ((error as any).code !== RESTJSONErrorCodes.UnknownMessage) throw error;
 				} finally {
-					if (!hasError)
-						msgs.filter((msg): msg is GuildMessage => !isNullOrUndefined(msg) && isGuildMessage(msg)).reduce(
-							(col, message) => col.set(message.id, message),
-							msgDeleted
+					if (!hasError) {
+						const messages = await Promise.all(
+							messagesBulked.filter(filterNullAndUndefined).map(async (msg) => (msg.partial ? msg.fetch() : msg))
 						);
+
+						const msgs = new Collection(messages.map((msg) => [msg.id, msg]));
+
+						msgs.reduce((col, message) => col.set(message.id, message), msgDeleted);
+					}
 				}
 			}
 		};
@@ -212,10 +219,9 @@ export class UserCommand extends WolfSubcommand {
 				toDelete.push(message.id);
 				remainingLimit--;
 			}
-			this.resolveKeys([...filtered.keys()], position, limit);
 
 			if (remainingLimit > 0 && filteredMessages.size > 0) {
-				await new Promise((resolve) => setTimeout(resolve, 2000));
+				await setTimeout(2000);
 				await fetchMessages(before && filteredMessages.last()?.id, after && filteredMessages.first()?.id);
 			}
 		};
