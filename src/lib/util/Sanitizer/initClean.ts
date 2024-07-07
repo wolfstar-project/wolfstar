5c9c320cf3b3051842d68c394df975dd86fa8839
RedStars071 <redtwoghost@gmail.com>
2024-07-07 16:40:55 +0200

    feat: add 'pgsql' suffix to secrets in  initclean file

--- src/lib/util/Sanitizer/initClean.ts
+++ src/lib/util/Sanitizer/initClean.ts
@@ -2,7 +2,7 @@ import { initClean } from '#utils/Sanitizer/clean';
 import { isNullishOrEmpty } from '@sapphire/utilities';
 
 const secrets = new Set<string>();
-const suffixes = ['_KEY', '_TOKEN', '_SECRET', '_PASSWORD'];
+const suffixes = ['_KEY', '_TOKEN', '_SECRET', '_PASSWORD', 'PGSQL'];
 for (const [key, value] of Object.entries(process.env)) {
 	if (isNullishOrEmpty(value)) continue;
 	if (suffixes.some((suffix) => key.endsWith(suffix))) secrets.add(value);
