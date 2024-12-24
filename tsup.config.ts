86ef03c21ffc8e91145d4cf5aea87789e1d46930
RedStars071 <redtwoghost@gmail.com>
2024-12-24 19:34:54 +0100

    chore: configure tsup for building and watching typescript files

---
+++ tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+	clean: true,
+	bundle: false,
+	dts: false,
+	entry: ['src/**/*.ts', '!src/**/*.d.ts'],
+	format: ['esm'],
+	minify: false,
+	tsconfig: 'tsconfig.json',
+	target: 'es2024',
+	splitting: false,
+	skipNodeModulesBundle: true,
+	sourcemap: true,
+	shims: false,
+	keepNames: true
+});
