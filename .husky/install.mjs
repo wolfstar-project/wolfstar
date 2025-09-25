8b91a80d47732ba928741b391139455bd025e659
RedStars071 <redtwoghost@gmail.com>
2025-09-25 17:43:26 +0200

    build: migrate from yarn to pnpm

---
+++ .husky/install.mjs
@@ -0,0 +1,8 @@
+/* eslint-disable antfu/no-top-level-await */
+
+// Skip Husky install in production and CI
+if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
+	process.exit(0);
+}
+const husky = (await import('husky')).default;
+console.log(husky());
