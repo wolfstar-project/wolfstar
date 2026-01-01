ab98ce5f4ead3c277a060c5c6fe43fc578d695ba
RedStar <redstar071@proton.me>
2026-01-01 18:22:04 +0100

    feat(Admin): enhance SettingsMenu with interactive command support and localized descriptions

--- src/commands/Admin/conf.ts
+++ src/commands/Admin/conf.ts
@@ -1,50 +1,125 @@
 import { getConfigurableGroups, isSchemaKey, readSettings, remove, reset, SchemaKey, set, writeSettingsTransaction } from '#lib/database';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { getSupportedUserLanguageT } from '#lib/i18n/translate';
 import { SettingsMenu, WolfSubcommand } from '#lib/structures';
-import type { GuildMessage } from '#lib/types';
 import { PermissionLevels } from '#lib/types';
 import { isValidCustomEmoji, isValidSerializedTwemoji, isValidTwemoji } from '#lib/util/functions/emojis';
-import { inlineCode } from '@discordjs/builders';
+import { chatInputApplicationCommandMention, inlineCode } from '@discordjs/builders';
 import { ApplyOptions, RequiresClientPermissions } from '@sapphire/decorators';
-import { CommandOptionsRunTypeEnum } from '@sapphire/framework';
+import { ApplicationCommandRegistry, CommandOptionsRunTypeEnum } from '@sapphire/framework';
+import { applyLocalizedBuilder } from '@sapphire/plugin-i18next';
 import { filter, map, toArray } from '@sapphire/iterator-utilities';
-import { send } from '@sapphire/plugin-editable-commands';
 import { isNullish, toTitleCase } from '@sapphire/utilities';
-import { PermissionFlagsBits } from 'discord.js';
+import { InteractionContextType, MessageFlags, PermissionFlagsBits } from 'discord.js';
+import { send } from '@sapphire/plugin-editable-commands';
+import type { GuildMessage } from 'lib/types/Discord.js';
+
+const Root = LanguageKeys.Commands.Admin;
 
 @ApplyOptions<WolfSubcommand.Options>({
-	aliases: ['settings', 'config', 'configs', 'configuration'],
-	description: LanguageKeys.Commands.Admin.ConfDescription,
-	detailedDescription: LanguageKeys.Commands.Admin.ConfExtended,
+	description: Root.ConfDescription,
+	detailedDescription: LanguageKeys.Commands.Shared.SlashOnlyDetailedDescription,
 	guarded: true,
 	permissionLevel: PermissionLevels.Administrator,
+	requiredClientPermissions: [PermissionFlagsBits.EmbedLinks],
 	runIn: [CommandOptionsRunTypeEnum.GuildAny],
+	hidden: true,
 	subcommands: [
-		{ name: 'set', messageRun: 'set' },
-		{ name: 'add', messageRun: 'set' },
-		{ name: 'show', messageRun: 'show' },
-		{ name: 'remove', messageRun: 'remove' },
-		{ name: 'reset', messageRun: 'reset' },
-		{ name: 'menu', messageRun: 'menu', default: true }
+		{ name: 'menu', chatInputRun: 'chatInputRunMenu', default: true },
+		{ name: 'show', chatInputRun: 'chatInputRunShow' },
+		{ name: 'set', chatInputRun: 'chatInputRunSet' },
+		{ name: 'remove', chatInputRun: 'chatInputRunRemove' },
+		{ name: 'reset', chatInputRun: 'chatInputRunReset' }
 	]
 })
 export class UserCommand extends WolfSubcommand {
+	public override registerApplicationCommands(registry: ApplicationCommandRegistry) {
+		registry.registerChatInputCommand((builder) =>
+			applyLocalizedBuilder(builder, Root.ConfDescription, Root.ConfDescription)
+				.setContexts(InteractionContextType.Guild)
+				.setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+				.addSubcommand(
+					(subcommand) => applyLocalizedBuilder(subcommand, Root.ConfMenuDescription) //
+				)
+				.addSubcommand((subcommand) =>
+					applyLocalizedBuilder(subcommand, Root.ConfShowDescription) //
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionKey).setRequired(false))
+				)
+				.addSubcommand((subcommand) =>
+					applyLocalizedBuilder(subcommand, Root.ConfSetDescription) //
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionKey).setRequired(true))
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionValue).setRequired(true))
+				)
+				.addSubcommand((subcommand) =>
+					applyLocalizedBuilder(subcommand, Root.ConfRemoveDescription) //
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionKey).setRequired(true))
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionValue).setRequired(true))
+				)
+				.addSubcommand((subcommand) =>
+					applyLocalizedBuilder(subcommand, Root.ConfResetDescription) //
+						.addStringOption((option) => applyLocalizedBuilder(option, Root.ConfOptionKey).setRequired(true))
+				)
+		);
+	}
+
 	@RequiresClientPermissions(PermissionFlagsBits.EmbedLinks)
-	public menu(message: GuildMessage, args: WolfSubcommand.Args, context: WolfSubcommand.RunContext) {
-		return new SettingsMenu(message, args.t).init(context);
+	public menu(message: GuildMessage, args: WolfSubcommand.Args) {
+		return send(message, {
+			content: args.t(LanguageKeys.Commands.Shared.DeprecatedMessage, {
+				command: chatInputApplicationCommandMention(this.name, 'show', this.getGlobalCommandId())
+			})
+		});
 	}
 
 	public async show(message: GuildMessage, args: WolfSubcommand.Args) {
-		const key = args.finished ? '' : await args.pick('string');
-		const schemaValue = getConfigurableGroups().getPathString(key.toLowerCase());
-		if (schemaValue === null) this.error(LanguageKeys.Commands.Admin.ConfGetNoExt, { key });
+		return send(message, {
+			content: args.t(LanguageKeys.Commands.Shared.DeprecatedMessage, {
+				command: chatInputApplicationCommandMention(this.name, 'show', this.getGlobalCommandId())
+			})
+		});
+	}
 
-		const settings = await readSettings(message.guild);
-		const output = schemaValue.display(settings, args.t);
+	public async set(message: GuildMessage, args: WolfSubcommand.Args) {
+		return send(message, {
+			content: args.t(LanguageKeys.Commands.Shared.DeprecatedMessage, {
+				command: chatInputApplicationCommandMention(this.name, 'show', this.getGlobalCommandId())
+			})
+		});
+	}
+
+	public async remove(message: GuildMessage, args: WolfSubcommand.Args) {
+		return send(message, {
+			content: args.t(LanguageKeys.Commands.Shared.DeprecatedMessage, {
+				command: chatInputApplicationCommandMention(this.name, 'show', this.getGlobalCommandId())
+			})
+		});
+	}
+
+	public async reset(message: GuildMessage, args: WolfSubcommand.Args) {
+		return send(message, {
+			content: args.t(LanguageKeys.Commands.Shared.DeprecatedMessage, {
+				command: chatInputApplicationCommandMention(this.name, 'show', this.getGlobalCommandId())
+			})
+		});
+	}
+
+	public async chatInputRunMenu(interaction: WolfSubcommand.Interaction) {
+		const t = getSupportedUserLanguageT(interaction);
+		return new SettingsMenu(interaction, t).init();
+	}
+
+	public async chatInputRunShow(interaction: WolfSubcommand.Interaction) {
+		const t = getSupportedUserLanguageT(interaction);
+		const key = interaction.options.getString('key') ?? '';
+		const schemaValue = getConfigurableGroups().getPathString(key.toLowerCase());
+		if (schemaValue === null) this.error(Root.ConfGetNoExt, { key });
+
+		const settings = await readSettings(interaction.guild);
+		const output = schemaValue.display(settings, t);
 
 		if (isSchemaKey(schemaValue)) {
-			const content = args.t(LanguageKeys.Commands.Admin.ConfGet, { key: schemaValue.name, value: output });
-			return send(message, { content, allowedMentions: { users: [], roles: [] } });
+			const content = t(Root.ConfGet, { key: schemaValue.name, value: output });
+			return interaction.reply({ content, allowedMentions: { users: [], roles: [] }, flags: MessageFlags.Ephemeral });
 		}
 
 		const title = key
@@ -53,61 +128,58 @@ export class UserCommand extends WolfSubcommand {
 					.map((key) => toTitleCase(key))
 					.join('/')}`
 			: '';
-		return send(message, {
-			content: args.t(LanguageKeys.Commands.Admin.Conf, { key: title, list: output }),
-			allowedMentions: { users: [], roles: [] }
-		});
+		const content = t(Root.Conf, { key: title, list: output });
+		return interaction.reply({ content, allowedMentions: { users: [], roles: [] }, flags: MessageFlags.Ephemeral });
 	}
 
-	public async set(message: GuildMessage, args: WolfSubcommand.Args) {
-		const [key, schemaKey] = await this.#fetchKey(args);
+	public async chatInputRunSet(interaction: WolfSubcommand.Interaction) {
+		const t = getSupportedUserLanguageT(interaction);
+		const [key, schemaKey] = await this.#fetchKeyFromInteraction(interaction, t);
 
-		using trx = await writeSettingsTransaction(message.guild);
-		await trx.write(await set(trx.settings, schemaKey, args)).submit();
+		using trx = await writeSettingsTransaction(interaction.guild);
+		const value = interaction.options.getString('value', true);
+		await trx.write(await set(trx.settings, schemaKey, { value, t })).submit();
 
-		const response = schemaKey.display(trx.settings, args.t);
-		return send(message, {
-			content: args.t(LanguageKeys.Commands.Admin.ConfUpdated, { key, response: this.#getTextResponse(response) }),
-			allowedMentions: { users: [], roles: [] }
-		});
+		const response = schemaKey.display(trx.settings, t);
+		const content = t(Root.ConfUpdated, { key, response: this.#getTextResponse(response) });
+		return interaction.reply({ content, allowedMentions: { users: [], roles: [] }, flags: MessageFlags.Ephemeral });
 	}
 
-	public async remove(message: GuildMessage, args: WolfSubcommand.Args) {
-		const [key, schemaKey] = await this.#fetchKey(args);
+	public async chatInputRunRemove(interaction: WolfSubcommand.Interaction) {
+		const t = getSupportedUserLanguageT(interaction);
+		const [key, schemaKey] = await this.#fetchKeyFromInteraction(interaction, t);
 
-		using trx = await writeSettingsTransaction(message.guild);
-		await trx.write(await remove(trx.settings, schemaKey, args)).submit();
+		using trx = await writeSettingsTransaction(interaction.guild);
+		const value = interaction.options.getString('value', true);
+		await trx.write(await remove(trx.settings, schemaKey, { value, t })).submit();
 
-		const response = schemaKey.display(trx.settings, args.t);
-		return send(message, {
-			content: args.t(LanguageKeys.Commands.Admin.ConfUpdated, { key, response: this.#getTextResponse(response) }),
-			allowedMentions: { users: [], roles: [] }
-		});
+		const response = schemaKey.display(trx.settings, t);
+		const content = t(Root.ConfUpdated, { key, response: this.#getTextResponse(response) });
+		return interaction.reply({ content, allowedMentions: { users: [], roles: [] }, flags: MessageFlags.Ephemeral });
 	}
 
-	public async reset(message: GuildMessage, args: WolfSubcommand.Args) {
-		const [key, schemaKey] = await this.#fetchKey(args);
+	public async chatInputRunReset(interaction: WolfSubcommand.Interaction) {
+		const t = getSupportedUserLanguageT(interaction);
+		const [key, schemaKey] = await this.#fetchKeyFromInteraction(interaction, t);
 
-		using trx = await writeSettingsTransaction(message.guild);
+		using trx = await writeSettingsTransaction(interaction.guild);
 		await trx.write(reset(schemaKey)).submit();
 
-		const response = schemaKey.display(trx.settings, args.t);
-		return send(message, {
-			content: args.t(LanguageKeys.Commands.Admin.ConfReset, { key, value: response }),
-			allowedMentions: { users: [], roles: [] }
-		});
+		const response = schemaKey.display(trx.settings, t);
+		const content = t(Root.ConfReset, { key, value: response });
+		return interaction.reply({ content, allowedMentions: { users: [], roles: [] }, flags: MessageFlags.Ephemeral });
 	}
 
 	#getTextResponse(response: string) {
 		return isValidCustomEmoji(response) || isValidSerializedTwemoji(response) || isValidTwemoji(response) ? response : inlineCode(response);
 	}
 
-	async #fetchKey(args: WolfSubcommand.Args) {
-		const key = await args.pick('string');
+	async #fetchKeyFromInteraction(interaction: WolfSubcommand.Interaction, t: ReturnType<typeof getSupportedUserLanguageT>) {
+		const key = interaction.options.getString('key', true);
 
 		const value = getConfigurableGroups().getPathString(key.toLowerCase());
 		if (isNullish(value) || value.dashboardOnly) {
-			this.error(LanguageKeys.Commands.Admin.ConfGetNoExt, { key });
+			this.error(Root.ConfGetNoExt, { key });
 		}
 
 		if (isSchemaKey(value)) {
--- src/lib/structures/SettingsMenu.ts
+++ src/lib/structures/SettingsMenu.ts
@@ -9,9 +9,9 @@ import {
 	set,
 	writeSettingsTransaction
 } from '#lib/database/settings';
-import { api } from '#lib/discord/Api';
 import { getT } from '#lib/i18n';
 import { LanguageKeys } from '#lib/i18n/languageKeys';
+import { LongLivingInteractionCollector } from '#lib/util/LongLivingInteractionCollector';
 import { WolfArgs, type WolfCommand } from '#lib/structures';
 import { Events, type GuildMessage } from '#lib/types';
 import { floatPromise, minutes, stringifyError } from '#utils/common';
@@ -30,16 +30,12 @@ import {
 import { container, type MessageCommand } from '@sapphire/framework';
 import { filter, partition } from '@sapphire/iterator-utilities';
 import type { TFunction } from '@sapphire/plugin-i18next';
-import { isNullish } from '@sapphire/utilities';
 import {
 	ButtonStyle,
 	ChannelType,
-	ComponentType,
 	DiscordAPIError,
-	InteractionCollector,
 	RESTJSONErrorCodes,
 	TextInputStyle,
-	type Interaction,
 	type Message,
 	type MessageActionRowComponentBuilder,
 	type MessageComponentInteraction,
@@ -59,7 +55,7 @@ export class SettingsMenu {
 	private readonly message: GuildMessage;
 	private t: TFunction;
 	private schema: SchemaKey | SchemaGroup;
-	private collector: InteractionCollector<any> | null = null;
+	private collector: LongLivingInteractionCollector | null = null;
 	private errorMessage: string | null = null;
 	private readonly embed: EmbedBuilder;
 	private response: Message | null = null;
@@ -86,13 +82,14 @@ export class SettingsMenu {
 		this.response = await sendLoadingMessage(this.message, this.t);
 		await this._renderResponse();
 
-		this.collector = this.response.createMessageComponentCollector({
-			filter: (i) => i.user.id === this.message.author.id,
-			time: TIMEOUT
+		this.collector = new LongLivingInteractionCollector();
+		this.collector.setListener((interaction) => {
+			if (interaction.message.id !== this.response?.id) return;
+			if (interaction.user.id !== this.message.author.id) return;
+			void this.onInteraction(interaction, context);
 		});
-
-		this.collector.on('collect', (i) => this.onInteraction(i, context));
-		this.collector.on('end', () => this.stop());
+		this.collector.setEndListener(() => this.stop());
+		this.collector.setTime(TIMEOUT);
 	}
 
 	private async render() {
@@ -152,7 +149,13 @@ export class SettingsMenu {
 		// Button Rows
 		const buttons = new ActionRowBuilder<MessageActionRowComponentBuilder>();
 		if (parent) {
-			buttons.addComponents(new ButtonBuilder().setCustomId('conf-back').setLabel(this.t(LanguageKeys.Globals.Back)).setStyle(ButtonStyle.Secondary).setEmoji('◀️'));
+			buttons.addComponents(
+				new ButtonBuilder()
+					.setCustomId('conf-back')
+					.setLabel(this.t(LanguageKeys.Globals.Back))
+					.setStyle(ButtonStyle.Secondary)
+					.setEmoji('◀️')
+			);
 		}
 
 		if (!isSchemaGroup(this.schema)) {
@@ -161,25 +164,38 @@ export class SettingsMenu {
 			const value = settings[this.schema.property];
 
 			// Set
-			buttons.addComponents(new ButtonBuilder().setCustomId('conf-set').setLabel(this.t(LanguageKeys.Globals.Set)).setStyle(ButtonStyle.Primary));
+			buttons.addComponents(
+				new ButtonBuilder().setCustomId('conf-set').setLabel(this.t(LanguageKeys.Globals.Set)).setStyle(ButtonStyle.Primary)
+			);
 
 			// Remove (if array and has elements)
 			if (this.schema.array && (value as unknown[]).length) {
-				buttons.addComponents(new ButtonBuilder().setCustomId('conf-remove').setLabel(this.t(LanguageKeys.Globals.Remove)).setStyle(ButtonStyle.Danger));
+				buttons.addComponents(
+					new ButtonBuilder().setCustomId('conf-remove').setLabel(this.t(LanguageKeys.Globals.Remove)).setStyle(ButtonStyle.Danger)
+				);
 			}
 
 			// Reset (if changed)
 			if (value !== this.schema.default) {
-				buttons.addComponents(new ButtonBuilder().setCustomId('conf-reset').setLabel(this.t(LanguageKeys.Globals.Reset)).setStyle(ButtonStyle.Danger));
+				buttons.addComponents(
+					new ButtonBuilder().setCustomId('conf-reset').setLabel(this.t(LanguageKeys.Globals.Reset)).setStyle(ButtonStyle.Danger)
+				);
 			}
 
 			// Undo (if updated)
 			if (this.updatedValue) {
-				buttons.addComponents(new ButtonBuilder().setCustomId('conf-undo').setLabel(this.t(LanguageKeys.Commands.Admin.ConfMenuRenderUndo)).setStyle(ButtonStyle.Secondary));
+				buttons.addComponents(
+					new ButtonBuilder()
+						.setCustomId('conf-undo')
+						.setLabel(this.t(LanguageKeys.Commands.Admin.ConfMenuRenderUndo))
+						.setStyle(ButtonStyle.Secondary)
+				);
 			}
 		}
 
-		buttons.addComponents(new ButtonBuilder().setCustomId('conf-stop').setLabel(this.t(LanguageKeys.Globals.Stop)).setStyle(ButtonStyle.Danger).setEmoji('⏹️'));
+		buttons.addComponents(
+			new ButtonBuilder().setCustomId('conf-stop').setLabel(this.t(LanguageKeys.Globals.Stop)).setStyle(ButtonStyle.Danger).setEmoji('⏹️')
+		);
 		rows.push(buttons);
 
 		return { embeds: [this.embed], components: rows };
@@ -200,46 +216,23 @@ export class SettingsMenu {
 				.setCustomId('conf-input-remove')
 				.setPlaceholder(this.t(LanguageKeys.Commands.Admin.ConfMenuRenderSelect));
 
-			// We need to stringify values to show labels, and use ID or value as value.
-			// Ideally we use serializer to stringify.
-			// If options > 25, we slice.
+			const options = await Promise.all(
+				values.map((val, index) => {
+					const label = key.stringify(settings, this.t, val).substring(0, 100);
+					// Better: For Roles/Channels, we can use their ID if available.
+					let valueId = String(val);
+					if (val && typeof val === 'object' && 'id' in val) {
+						// @ts-expect-error accessing id
+						valueId = val.id;
+					}
 
-			const options = await Promise.all(values.map(async (val, index) => {
-				const label = key.stringify(settings, this.t, val).substring(0, 100);
-				// We use the serialized value as the 'value' to pass to WolfArgs, assuming WolfArgs can parse it back.
-				// However, WolfArgs parses input string. Serializer stringifies it to human readable.
-				// For Roles/Channels, stringify usually gives name or mention.
-				// For ID-based parsing, we need the ID.
-				// This is tricky. simpler approach: Just show indices? No, user doesn't know indices.
-
-				// Better: For Roles/Channels, we can use their ID if available.
-				let valueId = String(val);
-				if (val && typeof val === 'object' && 'id' in val) {
-					// @ts-expect-error accessing id
-					valueId = val.id;
-				} else {
-					// Try to use the raw value if it's primitive.
-					// If it's a string, it might have spaces.
-				}
-
-				// Fallback: Use the serialized string and hope WolfArgs can resolve it (e.g. by name).
-				// Or, use the index and let 'remove' handle index?
-				// Database 'remove' function uses `serializer.equals` finding the value.
-				// If we pass the EXACT value via WolfArgs, it should work.
-				// But we are passing a string.
-				// Let's stick to showing the Remove Button (which we did) which leads here.
-
-				// Actually, for arrays, showing a list to remove is best.
-				// But constructing the correct string for WolfArgs to recreate the object is hard for complex types.
-				// For Roles, Channels, Users: ID is best.
-				// For Strings: The string itself.
-
-				return {
-					label: label || 'Unknown',
-					value: valueId.substring(0, 100),
-					description: String(index)
-				};
-			}));
+					return {
+						label: label || 'Unknown',
+						value: valueId.substring(0, 100),
+						description: String(index)
+					};
+				})
+			);
 
 			if (options.length) {
 				selectMenu.addOptions(options.slice(0, 25));
@@ -252,10 +245,18 @@ export class SettingsMenu {
 		else {
 			switch (key.type) {
 				case 'boolean': {
-					rows.push(new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-						new ButtonBuilder().setCustomId('conf-input-bool-true').setLabel(this.t(LanguageKeys.Globals.Yes)).setStyle(ButtonStyle.Success),
-						new ButtonBuilder().setCustomId('conf-input-bool-false').setLabel(this.t(LanguageKeys.Globals.No)).setStyle(ButtonStyle.Danger)
-					));
+					rows.push(
+						new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+							new ButtonBuilder()
+								.setCustomId('conf-input-bool-true')
+								.setLabel(this.t(LanguageKeys.Globals.Yes))
+								.setStyle(ButtonStyle.Success),
+							new ButtonBuilder()
+								.setCustomId('conf-input-bool-false')
+								.setLabel(this.t(LanguageKeys.Globals.No))
+								.setStyle(ButtonStyle.Danger)
+						)
+					);
 					break;
 				}
 				case 'role': {
@@ -277,24 +278,15 @@ export class SettingsMenu {
 					rows.push(new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(select));
 					break;
 				}
-				// For others, we might have used Modal, but since we are in `renderInput`,
-				// if we want to support Modal for others, we should have triggered it immediately in `onInteraction`.
-				// So if we are here, it means we don't use Modal, OR we show a "Click to Enter Value" button which opens Modal.
-				// But that adds a step.
-				// Better: In `onInteraction`, if type is string/number, open Modal immediately and don't enter `inputMode` unless we want to show error state.
-				default: {
-					// Fallback to button that opens modal (if we ended up here)
-					rows.push(new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-						new ButtonBuilder().setCustomId('conf-input-modal').setLabel(this.t(LanguageKeys.Globals.Set)).setStyle(ButtonStyle.Primary)
-					));
-				}
 			}
 		}
 
 		// Cancel Button
-		rows.push(new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
-			new ButtonBuilder().setCustomId('conf-cancel').setLabel(this.t(LanguageKeys.Globals.Cancel)).setStyle(ButtonStyle.Secondary)
-		));
+		rows.push(
+			new ActionRowBuilder<MessageActionRowComponentBuilder>().addComponents(
+				new ButtonBuilder().setCustomId('conf-cancel').setLabel(this.t(LanguageKeys.Globals.Cancel)).setStyle(ButtonStyle.Secondary)
+			)
+		);
 
 		return { embeds: [this.embed], components: rows };
 	}
@@ -342,7 +334,7 @@ export class SettingsMenu {
 	}
 
 	private async onInteraction(interaction: MessageComponentInteraction, context: WolfCommand.RunContext) {
-		this.collector?.resetTimer();
+		this.collector?.setTime(TIMEOUT);
 		this.errorMessage = null;
 
 		// Handle Select Menu for Navigation
@@ -492,9 +484,13 @@ export class SettingsMenu {
 			await submission.deferUpdate();
 
 			await this.processUpdate(type, value, context);
-
 		} catch (error) {
-			if (error instanceof Error && error.message.includes('time')) {
+			if (
+				error instanceof Error &&
+				(error.name === 'InteractionCollectorError' ||
+					error.name === 'Error [InteractionCollectorError]' ||
+					error.message === 'Collector received no interactions before ending with reason: time')
+			) {
 				// Ignore timeout
 			} else {
 				this.errorMessage = stringifyError(this.t, error);
@@ -519,7 +515,7 @@ export class SettingsMenu {
 		} catch (error) {
 			if (error instanceof DiscordAPIError && error.code === RESTJSONErrorCodes.UnknownMessage) {
 				this.response = null;
-				this.collector?.stop();
+				this.collector?.end();
 			} else {
 				this.client.emit(Events.Error, error as Error);
 			}
@@ -573,7 +569,7 @@ export class SettingsMenu {
 		}
 
 		if (this.collector && !this.collector.ended) {
-			this.collector.stop();
+			this.collector.end();
 		}
 	}
 }
