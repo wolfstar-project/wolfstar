dbf30b013d7e76050cd6f5cc49189b48ed4105e2
RedStar <redstar071@proton.me>
2026-01-17 15:20:31 +0100

    refactor(i18n): replace LocaleString with Locale type for consistency

--- src/arguments/language.ts
+++ src/arguments/language.ts
@@ -1,11 +1,11 @@
 import { LanguageKeys } from '#lib/i18n/languageKeys';
 import { Argument } from '@sapphire/framework';
-import type { LocaleString } from 'discord.js';
+import type { Locale } from 'discord.js';
 
-export class UserArgument extends Argument<LocaleString> {
+export class UserArgument extends Argument<Locale> {
 	public run(parameter: string, context: Argument.Context) {
 		const { languages } = this.container.i18n;
-		if (languages.has(parameter)) return this.ok(parameter as LocaleString);
+		if (languages.has(parameter)) return this.ok(parameter as Locale);
 		return this.error({ parameter, identifier: LanguageKeys.Arguments.Language, context: { ...context, possibles: [...languages.keys()] } });
 	}
 }
