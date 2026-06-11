f7b81a2ccbe42e6cf7eb7bc874bbd655b4187acf
RedStar <redstar071@proton.me>
2026-06-11 13:56:32 +0200

    refactor: update command success analytics listeners to use async settings retrieval

--- src/listeners/commands/chatInputCommandSuccessAnalytics.ts
+++ src/listeners/commands/chatInputCommandSuccessAnalytics.ts
@@ -1,19 +1,18 @@
 import type { WolfCommand } from '#lib/structures';
-import { readSettingsAuditLog, readSettingsCached } from '#lib/database';
+import { readSettings, readSettingsAuditLog } from '#lib/database';
 import { Events as WolfEvents } from '#lib/types';
 import { ApplyOptions } from '@sapphire/decorators';
 import { Events, Listener, type ChatInputCommandSuccessPayload } from '@sapphire/framework';
 
 @ApplyOptions<Listener.Options>({ event: Events.ChatInputCommandSuccess })
 export class UserListener extends Listener<typeof Events.ChatInputCommandSuccess> {
-	public run(payload: ChatInputCommandSuccessPayload) {
+	public async run(payload: ChatInputCommandSuccessPayload) {
 		const command = payload.command as WolfCommand;
 		const { interaction } = payload;
 		this.container.client.emit(WolfEvents.CommandUsageAnalytics, command.name, command.category);
 
 		if (!interaction.guildId || command.category === 'System') return;
-		const settings = readSettingsCached(interaction.guildId);
-		if (!settings) return;
+		const settings = await readSettings(interaction.guildId);
 
 		const subcommandGroup = interaction.options.getSubcommandGroup(false);
 		const subcommand = interaction.options.getSubcommand(false);
--- src/listeners/commands/messageCommandSuccessAnalytics.ts
+++ src/listeners/commands/messageCommandSuccessAnalytics.ts
@@ -1,19 +1,18 @@
 import type { WolfCommand } from '#lib/structures';
 import { Events as WolfEvents } from '#lib/types';
 import { ApplyOptions } from '@sapphire/decorators';
-import { readSettingsAuditLog, readSettingsCached } from '#lib/database';
+import { readSettings, readSettingsAuditLog } from '#lib/database';
 import { Events, Listener, type MessageCommandSuccessPayload } from '@sapphire/framework';
 
 @ApplyOptions<Listener.Options>({ event: Events.MessageCommandSuccess })
 export class UserListener extends Listener<typeof Events.MessageCommandSuccess> {
-	public run(payload: MessageCommandSuccessPayload) {
+	public async run(payload: MessageCommandSuccessPayload) {
 		const command = payload.command as WolfCommand;
 		const { message } = payload;
 		this.container.client.emit(WolfEvents.CommandUsageAnalytics, command.name, command.category);
 
 		if (!message.guildId || command.category === 'System') return;
-		const settings = readSettingsCached(message.guildId);
-		if (!settings) return;
+		const settings = await readSettings(message.guildId);
 		void readSettingsAuditLog(settings)
 			.command(message.author.id, {
 				commandName: command.name,
